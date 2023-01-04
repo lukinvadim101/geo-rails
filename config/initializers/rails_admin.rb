a2adc0ed881bb4cc1ff744c5e0635374a42b6c61
lukinvadim101 <71687637+lukinvadim101@users.noreply.github.com>
2023-01-04 11:11:40 -0600

    admin panel notification

--- config/initializers/rails_admin.rb
+++ config/initializers/rails_admin.rb
@@ -3,7 +3,7 @@ RailsAdmin.config do |config|
 
   ## == Devise ==
   config.authenticate_with do
-    redirect_to '/' unless warden.user.admin?
+    redirect_to '/', notice: 'Got no permissions to admin panel access' unless warden.user.admin?
   end
   config.current_user_method(&:current_user)
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
   devise_for :users
+
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
-  get 'home/index'
+
   root 'home#index'
 end
 
